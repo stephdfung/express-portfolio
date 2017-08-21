6125abb493596225977e5a921ca1727c14f050e7
Stephanie Fung <stephaniedfung@gmail.com>
2017-08-21 11:56:30 -0400

    Nodemon and ej installed

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World! - update')
+})
+
+app.get('/about', function (req, res) {
+  res.send('This is the about page - update')
 })
 
 app.listen(3000, function () {
