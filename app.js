385bfd2c10ba35bff82299678f22673a5be652c6
Stephanie Fung <stephaniedfung@gmail.com>
2017-08-21 12:25:32 -0400

    Loaded portfolio and static css

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static('public'))
+
 app.get('/', function (req, res) {
-  res.send('Hello World! - update')
+  // res.send('Hello World! - update')
+  res.render('index')
 })
 
 app.get('/about', function (req, res) {
-  res.send('This is the about page - update')
+  // res.send('This is the about page - update')
+  res.render('about')
 })
 
 app.listen(3000, function () {
